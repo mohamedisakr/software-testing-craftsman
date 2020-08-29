6e6bc6702ad0799c7323a38995c5e2add7f8979b
mohamedisakr <mohamedisakr@gmail.com>
2020-08-29 14:17:32 +0200

    implement computeCommission function

--- commission-problem.js
+++ commission-problem.js
@@ -2,8 +2,8 @@ const lockUnitPrice = 45;
 const stockUnitPrice = 30;
 const barrelUnitPrice = 25;
 
-// validation 
-/* 
+
+/* // validation 
     1 <= locks <= 70
     1 <= stocks <= 80
     1 <= barrels <= 90
@@ -30,6 +30,32 @@ function validMaxBarrels(barrels) {
     return false;
 }
 
+/* 
+    10% on sales up to (and including) $1000, 
+    15% on the next $800, 
+    20% on any sales in excess of $1800.
+*/
+function computeCommission(amount) {    
+    let commission = 0.0;
+  
+   if(amount > 1800) {
+       commission = amount * 0.2;
+   } else { 
+       const rest = amount - 1000;
+        if(rest > 0) {
+           commission = (1000 * 0.1) + (rest * 0.15)
+        } else {
+            commission = amount * 0.1;
+        }
+   }
+   return commission
+}
+
+const amounts = [700, 1000, 1790, 1800, 2000];
+//              [70,  100,  218.5,220,  400]
+amounts.forEach(amount=>console.log(computeCommission(amount)))
+
+/*
 // const locks = [0, 1, 2, 69, 70, 71];
 // locks.forEach(lock=>console.log(validMaxLocks(lock)));
 
@@ -38,3 +64,4 @@ function validMaxBarrels(barrels) {
 
 // const barrels = [0, 1, 2, 89, 90, 91];
 // barrels.forEach(barrel=>console.log(validMaxBarrels(barrel)));
+*/
