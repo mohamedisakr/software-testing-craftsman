332d60ef104fbb59043b31f820844d9318abec19
mohamedisakr <mohamedisakr@gmail.com>
2020-09-12 10:50:50 +0200

    adding cross product function

--- robust-worst-case.js
+++ robust-worst-case.js
@@ -6,3 +6,22 @@ function generateRobustBoundaryValue(min, max) {
   const nom = parseInt(max / 2);
   return [min - 1, min, min + 1, nom, max - 1, max, max + 1];
 }
+
+function generateProduct(...arrays) {
+  let data = [].concat(arrays);
+  return data.reduce((acc, curr) =>
+    acc.flatMap((c) => curr.map((n) => [].concat(c, n)))
+  );
+}
+
+let daysCases = generateRobustBoundaryValue(1, 31);
+// console.log(daysCases);
+
+let monthsCases = generateRobustBoundaryValue(1, 12);
+// console.log(monthsCases);
+
+let yearsCases = generateRobustBoundaryValue(1812, 2012);
+// console.log(yearsCases);
+
+const product = generateProduct(daysCases, monthsCases, yearsCases);
+console.log(product);
