bb8bfa349fa795dcada7eb7c17a295c140f1c9b0
mohamedisakr <mohamedisakr@gmail.com>
2020-10-13 12:41:23 +0200

    adding more async function tests

--- js/async-function.js
+++ js/async-function.js
@@ -2,4 +2,17 @@ async function add(x, y) {
   return (await x) + y;
 }
 
-module.exports = { add };
+async function gcd(x, y) {
+  while (x !== y) {
+    if (x > y) {
+      x -= y;
+      //   x /= y;
+    } else {
+      y -= x;
+      //   y /= x;
+    }
+  }
+  return await x;
+}
+
+module.exports = { add, gcd };
--- tests/async-function.test.js
+++ tests/async-function.test.js
@@ -1,9 +1,36 @@
 const { add } = require("../js/async-function");
+const { gcd } = require("../number-theory");
 
 describe("test async function", () => {
   test("add function async", async () => {
     const expected = 28;
     const actual = await add(21, 7);
+    expect(actual).toBe(expected);
+  });
+});
+
+describe("do statement coverage", () => {
+  test("x > y", async () => {
+    const expected = 7;
+    const actual = await gcd(21, 7);
+    expect(actual).toBe(expected);
+  });
+
+  test("x < y", async () => {
+    const expected = 7;
+    const actual = await gcd(7, 21);
+    expect(actual).toBe(expected);
+  });
+
+  test("x = y", async () => {
+    const expected = 7;
+    const actual = await gcd(7, 7);
+    expect(actual).toBe(expected);
+  });
+
+  test("x != y & gcd = 1", async () => {
+    const expected = 1;
+    const actual = await gcd(12, 7);
     expect(actual).toBe(expected);
   });
 });
