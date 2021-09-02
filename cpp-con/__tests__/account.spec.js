e5b115bd5d0e2a0709a01b7384c0b0ebee85d69c
mohamedisakr <mohamedisakr@gmail.com>
2021-09-02 20:47:18 +0200

    implementing additional basic bank account test cases

--- cpp-con/__tests__/account.spec.js
+++ cpp-con/__tests__/account.spec.js
@@ -1,13 +1,34 @@
 const Account = require("../account");
 
 describe("Account class", () => {
-  it("should initialize with balance = 0", () => {
+  it("A new account should has an empty balance", () => {
     const account = new Account(0);
     expect(account.getBalance()).toEqual(0);
   });
 
-  it.skip("should initialize the account class", () => {
+  it("An empty account can be deposit to", () => {
+    const amount = 100;
     const account = new Account(0);
-    expect(account).toHaveBeenCalledTimes(1);
+    account.deposit(amount);
+    expect(account.getBalance()).toEqual(amount);
+  });
+
+  it("An empty account can be withdraw from with negative balance", () => {
+    const amount = 100;
+    const actual = -110;
+    const account = new Account(0);
+    account.withdraw(amount);
+
+    expect(account.getBalance()).toEqual(actual);
   });
 });
+
+// it("should initialize with balance = 0", () => {
+//   const account = new Account(0);
+//   expect(account.getBalance()).toEqual(0);
+// });
+
+// it.skip("should initialize the account class", () => {
+//   const account = new Account(0);
+//   expect(account).toHaveBeenCalledTimes(1);
+// });
