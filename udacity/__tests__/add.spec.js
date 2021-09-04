1908fa1775ebbc052bd06f9370076c3a319f95e9
mohamedisakr <mohamedisakr@gmail.com>
2021-09-04 20:31:52 +0200

    test whether a particular function throws an error when it's called

--- udacity/__tests__/add.spec.js
+++ udacity/__tests__/add.spec.js
@@ -12,29 +12,31 @@ describe("add function NORMAL test suite", () => {
 });
 
 describe("add function EDGE test suite", () => {
-  edgeTestCases.forEach((testCase) => {
-    const { description, num1, num2, actual } = testCase;
-    it(`${description}`, () => {
-      expect.assertions(1);
+  // edgeTestCases.forEach((testCase) => {
+  //   const { description, num1, num2, actual } = testCase;
+  //   it(`${description}`, () => {
+  //     expect.assertions(1);
 
-      try {
-        const expected = add(num1, num2);
-        expect(expected).toEqual(actual);
-      } catch (e) {
-        expect(e).toBeTruthy();
-      }
-    });
-  });
+  //     try {
+  //       const expected = add(num1, num2);
+  //       expect(expected).toEqual(actual);
+  //     } catch (e) {
+  //       expect(e).toBeTruthy();
+  //     }
+  //   });
+  // });
 
   //   it("What about multiple expect() function in 1 test() / it() block", () => {
   //     expect(add(1, 2)).toEqual(3);
   //     expect(add(1, 2)).not.toEqual(3);
   //   });
 
-  //   edgeTestCases.forEach((testCase) => {
-  //     const { description, num1, num2, actual } = testCase;
-  //     it(`${description}`, () => {
-  //       expect(add(num1, num2)).toThrow();
-  //     });
-  //   });
+  // // You can also use the exact error message or a regexp
+  // expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+  edgeTestCases.forEach((testCase) => {
+    const { description, num1, num2, actual } = testCase;
+    it(`${description}`, () => {
+      expect(() => add(num1, num2)).toThrow("num1 & num2 must be numbers");
+    });
+  });
 });
