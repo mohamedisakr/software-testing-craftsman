70dcedd638ff42d0e97ee7f28e1cd244d56fc7dd
mohamedisakr <mohamedisakr@gmail.com>
2021-09-09 20:19:46 +0200

    refactoring udacity test cases to use data driven testing

--- udacity/__tests__/add.spec.js
+++ udacity/__tests__/add.spec.js
@@ -1,14 +1,12 @@
 const add = require("../add");
 const {
-  testCases,
   normalTestCases,
   decimalTestCase,
   edgeTestCases,
 } = require("../fixtures/test-data");
 
-describe.only("mixed test cases", () => {
-  // description: "should add integers", num1: 1, num2: 2, actual: 3
-  test.each(testCases)(
+describe("NORMAL test suite - add function ", () => {
+  test.each(normalTestCases)(
     '"%s"  %d + %d = %d',
     (description, num1, num2, actual) => {
       const expected = add(num1, num2);
@@ -17,52 +15,72 @@ describe.only("mixed test cases", () => {
   );
 });
 
-describe("NORMAL test suite - add function ", () => {
-  normalTestCases.forEach((testCase) => {
-    const { description, num1, num2, actual } = testCase;
-    it(`${description}`, () => {
-      const expected = add(num1, num2);
-      expect(expected).toEqual(actual);
-    });
-  });
-});
-
 describe("DECIMAL test suite - add function", () => {
-  decimalTestCase.forEach((testCase) => {
-    const { description, num1, num2, actual } = testCase;
-    it(`${description}`, () => {
+  test.each(decimalTestCase)(
+    '"%s"  %d + %d = %d',
+    (description, num1, num2, actual) => {
       const expected = add(num1, num2);
       expect(expected).toBeCloseTo(actual);
-    });
-  });
+    }
+  );
 });
 
 describe("EDGE test suite - add function", () => {
-  // edgeTestCases.forEach((testCase) => {
-  //   const { description, num1, num2, actual } = testCase;
-  //   it(`${description}`, () => {
-  //     expect.assertions(1);
-
-  //     try {
-  //       const expected = add(num1, num2);
-  //       expect(expected).toEqual(actual);
-  //     } catch (e) {
-  //       expect(e).toBeTruthy();
-  //     }
-  //   });
-  // });
-
-  //   it("What about multiple expect() function in 1 test() / it() block", () => {
-  //     expect(add(1, 2)).toEqual(3);
-  //     expect(add(1, 2)).not.toEqual(3);
-  //   });
-
-  // // You can also use the exact error message or a regexp
+  //  You can also use the exact error message or a regexp
   // expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
-  edgeTestCases.forEach((testCase) => {
-    const { description, num1, num2, actual } = testCase;
-    it(`${description}`, () => {
+  test.each(edgeTestCases)(
+    '"%s"  %d + %d = %d',
+    (description, num1, num2, actual) => {
       expect(() => add(num1, num2)).toThrow("num1 & num2 must be numbers");
-    });
-  });
+    }
+  );
 });
+
+//#region refactored code
+
+// normalTestCases.forEach((testCase) => {
+//   const { description, num1, num2, actual } = testCase;
+//   it(`${description}`, () => {
+//     const expected = add(num1, num2);
+//     expect(expected).toEqual(actual);
+//   });
+// });
+// -----------------------------------------------------------------------------
+// describe.only("mixed test cases", () => {
+//   // description: "should add integers", num1: 1, num2: 2, actual: 3
+//   test.each(testCases)(
+//     '"%s"  %d + %d = %d',
+//     (description, num1, num2, actual) => {
+//       const expected = add(num1, num2);
+//       expect(expected).toEqual(actual);
+//     }
+//   );
+// });
+// -----------------------------------------------------------------------------
+// edgeTestCases.forEach((testCase) => {
+//   const { description, num1, num2, actual } = testCase;
+//   it(`${description}`, () => {
+//     expect.assertions(1);
+
+//     try {
+//       const expected = add(num1, num2);
+//       expect(expected).toEqual(actual);
+//     } catch (e) {
+//       expect(e).toBeTruthy();
+//     }
+//   });
+// });
+// -----------------------------------------------------------------------------
+//   it("What about multiple expect() function in 1 test() / it() block", () => {
+//     expect(add(1, 2)).toEqual(3);
+//     expect(add(1, 2)).not.toEqual(3);
+//   });
+// -----------------------------------------------------------------------------
+// edgeTestCases.forEach((testCase) => {
+//   const { description, num1, num2, actual } = testCase;
+//   it(`${description}`, () => {
+//     expect(() => add(num1, num2)).toThrow("num1 & num2 must be numbers");
+//   });
+// });
+
+//#endregion
--- udacity/fixtures/test-data.js
+++ udacity/fixtures/test-data.js
@@ -1,24 +1,10 @@
 const normalTestCases = [
-  { description: "should add integers", num1: 1, num2: 2, actual: 3 },
-];
-
-const decimalTestCase = [
-  { description: "should add decimals", num1: 0.1, num2: 0.2, actual: 0.3 },
-];
-
-const edgeTestCases = [
-  {
-    description: "should throw error if strings",
-    num1: "1",
-    num2: "2",
-    actual: "3",
-  },
-];
-
-const testCases = [
+  // { description: "should add integers", num1: 1, num2: 2, actual: 3 },
   ["should add integers", 1, 2, 3],
-  // ["should add decimals", 0.1, 0.2, 0.3],
-  // ["should throw error if strings", "1", "2", "3"],
 ];
 
-module.exports = { testCases, normalTestCases, decimalTestCase, edgeTestCases };
+const decimalTestCase = [["should add decimals", 0.1, 0.2, 0.3]];
+
+const edgeTestCases = [["should throw error if strings", "1", "2", "3"]];
+
+module.exports = { normalTestCases, decimalTestCase, edgeTestCases };
