b0c75910fb0bf861f7439d2fcdab333a974ee075
mohamedisakr <mohamedisakr@gmail.com>
2021-09-11 09:18:52 +0200

    implementing step 3

--- kent/__tests__/thumb-war.spec.js
+++ kent/__tests__/thumb-war.spec.js
@@ -3,17 +3,18 @@ const thumbWar = require("../thumb-war");
 // const utils = require("../utils");
 
 test("returns winner", () => {
-  const getWinner = (...args) => {
-    getWinner.mock.calls.push(args);
-    return args[1];
-  };
+  const getWinner = jest.fn((p1, p2) => p2);
 
   getWinner.mock = { calls: [] };
 
   const winner = thumbWar("Ken Wheeler", "Kent C. Dodds");
-  expect(winner).toBe("Kent C. Dodds");
-  expect(getWinner.mock.calls).toHaveLength(2);
-  getWinner.mock.calls.forEach((args) => {
-    expect(args).toEqual(["Ken Wheeler", "Kent C. Dodds"]);
-  });
+  // expect(winner).toBe("Kent C. Dodds");
+  expect(getWinner).toBeCalledTimes(2);
+
+  expect(getWinner).nthCalledWith(1, "Ken Wheeler");
+  expect(getWinner).nthCalledWith(2, "Kent C. Dodds");
 });
+
+// getWinner.mock.calls.forEach((args) => {
+//   expect(args).toEqual(["Ken Wheeler", "Kent C. Dodds"]);
+// });
