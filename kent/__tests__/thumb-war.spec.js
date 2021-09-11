75793a267402b7d3cd1b1513691ae99dbff4c6a9
mohamedisakr <mohamedisakr@gmail.com>
2021-09-11 09:51:30 +0200

    implementing step 5

--- kent/__tests__/thumb-war.spec.js
+++ kent/__tests__/thumb-war.spec.js
@@ -1,13 +1,18 @@
 const thumbWar = require("../thumb-war");
 const utils = require("../utils");
-// const utils = require("../utils");
+const utilsMock = require("../utils");
+
+jest.mock("../utils", () => {
+  return {
+    getWinner: jest.fn((p1, p2) => p2),
+  };
+});
 
 test("returns winner", () => {
-  jest.spyOn(utils, "getWinner");
-  utils.getWinner.mockImplementation((p1, p2) => p2);
-
   const winner = thumbWar("Ken Wheeler", "Kent C. Dodds");
   expect(winner).toBe("Kent C. Dodds");
-
-  utils.getWinner.mockRestore();
+  expect(utilsMock.getWinner).toBeCalledTimes(2);
+  utilsMock.getWinner.mock.calls.forEach((args) => {
+    expect(args).toEqual(["Ken Wheeler", "Kent C. Dodds"]);
+  });
 });
